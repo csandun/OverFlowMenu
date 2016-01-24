40e8b03e1fc79eeb086778cce705c9049729a77f
chathurangasandun <chathurangasandukumara@gmail.com>
2016-01-24 19:58:36 +0530

    Initial commit

---
+++ app/src/main/java/com/clivekumara/overflowmenu/MainActivity.java
@@ -0,0 +1,80 @@
+package com.clivekumara.overflowmenu;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainlayout);
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.blue) {
+            if(item.isChecked()){
+                item.setChecked(false);
+            }else{
+                item.setChecked(true);
+            }
+
+            mainLayout.setBackgroundColor(Color.BLUE);
+            return true;
+        }else if (id == R.id.green) {
+            if(item.isChecked()){
+                item.setChecked(false);
+            }else{
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.GREEN);
+            return true;
+        } else  if (id == R.id.red) {
+            if(item.isChecked()){
+                item.setChecked(false);
+            }else{
+                item.setChecked(true);
+            }
+            mainLayout.setBackgroundColor(Color.RED);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
